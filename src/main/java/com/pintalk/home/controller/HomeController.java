c87e631da5ae46e6f945c1e156975084c9a9166b
top <dks2922@gmail.com>
2022-12-20 21:43:41 +0900

    chore : 패키지명 수정

--- src/main/java/com/pintalk/home/controller/HomeController.java
+++ src/main/java/com/pintalk/home/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.pintalk.home.Controller;
+package com.pintalk.home.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
